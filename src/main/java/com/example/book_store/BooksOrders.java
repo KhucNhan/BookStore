8bc89fa8d0a0b93505478d5b52b03bf2e764dd50
Khuc Nhan <khucvii@gmail.com>
2024-10-15 16:37:37 +0700

    create objects

---
+++ src/main/java/com/example/book_store/BooksOrders.java
@@ -0,0 +1,47 @@
+package com.example.book_store;
+
+public class BooksOrders {
+    private int orderID;
+    private int bookID;
+    private int amount;
+
+    public BooksOrders(int orderID, int bookID, int amount) {
+        this.orderID = orderID;
+        this.bookID = bookID;
+        this.amount = amount;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "BooksOrders{" +
+                "orderID=" + orderID +
+                ", bookID=" + bookID +
+                ", amount=" + amount +
+                '}';
+    }
+}
+
