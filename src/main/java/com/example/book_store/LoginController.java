13c788a35f612b6bf9babceb79cb6dfddb92fad6
Khuc Nhan <khucvii@gmail.com>
2024-10-15 21:39:49 +0700

    feature login logic

--- src/main/java/com/example/book_store/LoginController.java
+++ src/main/java/com/example/book_store/LoginController.java
@@ -37,10 +37,8 @@ public class LoginController {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
-        // Xử lý đăng nhập
         if (validateLogin(username, password)) {
             showAlert(Alert.AlertType.INFORMATION, "Đăng nhập thành công", "Xin chào, " + username + "!");
-
         } else {
             showAlert(Alert.AlertType.ERROR, "Đăng nhập thất bại", "Tên đăng nhập hoặc mật khẩu không chính xác.");
         }
@@ -49,29 +47,27 @@ public class LoginController {
 
 
     private boolean validateLogin(String username, String password) {
-//        String query = "SELECT * FROM users WHERE username = ? AND password = ?";
-//
-//        try (Connection conn = connect();
-//             PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-//
-//            preparedStatement.setString(1, username);
-//            preparedStatement.setString(2, password);
-//
-//            ResultSet resultSet = preparedStatement.executeQuery();
-//
-//            // Kiểm tra kết quả trả về
-//            return resultSet.next(); // Nếu có kết quả trả về, đăng nhập hợp lệ
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            showAlert(Alert.AlertType.ERROR, "Lỗi", "Không thể kết nối cơ sở dữ liệu.");
-//        }
+        String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+        ConnectDB connectDB = new ConnectDB();
+        try {
+            Connection conn = connectDB.connectionDB();
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            return resultSet.next();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            showAlert(Alert.AlertType.ERROR, "Lỗi", "Không thể kết nối cơ sở dữ liệu.");
+        }
         return true;
     }
 
     @FXML
     private void handleForgotPasswordLink() {
-        // Xử lý quên mật khẩu
         showAlert(Alert.AlertType.INFORMATION, "Quên mật khẩu", "Vui lòng liên hệ quản trị viên để đặt lại mật khẩu.");
     }
 
