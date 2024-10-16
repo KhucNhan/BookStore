73701ce821b7b33eadd321f95526edfca3a1bb44
Khuc Nhan <khucvii@gmail.com>
2024-10-16 12:48:18 +0700

    alert and confirmation feature

--- src/main/java/com/example/book_store/LoginController.java
+++ src/main/java/com/example/book_store/LoginController.java
@@ -22,6 +22,7 @@ import java.sql.SQLException;
 
 public class LoginController {
     private final ConnectDB connectDB = new ConnectDB();
+    Connection connection = connectDB.connectionDB();
 
     @FXML
     private TextField usernameField;
@@ -44,19 +45,22 @@ public class LoginController {
         // Khởi tạo nếu cần thiết
     }
 
+    public static User currentUser;
+
     @FXML
     private void handleLogin(ActionEvent event) throws IOException {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
         if (validateLogin(username, password) && isActive(username, password)) {
-            showAlert(Alert.AlertType.INFORMATION, "Đăng nhập thành công", "Xin chào, " + username + "!");
+            currentUser = getUSerByUserNameAndPassword(username, password);
+            showAlert(Alert.AlertType.INFORMATION, "Đăng nhập thành công", "Xin chào " + currentUser.getRole() + " " + currentUser.getName() + "!");
             Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
             stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             scene = new Scene(root, 720, 480);
             stage.setScene(scene);
             stage.show();
-        } else if (validateLogin(username, password) && !isActive(username,password)){
+        } else if (validateLogin(username, password) && !isActive(username, password)) {
             showAlert(Alert.AlertType.ERROR, "Đăng nhập thất bại", "Tài khoản đã bị hủy.");
         } else {
             showAlert(Alert.AlertType.ERROR, "Đăng nhập thất bại", "Tên đăng nhập hoặc mật khẩu không chính xác.");
@@ -73,10 +77,27 @@ public class LoginController {
         return login(username, password, query);
     }
 
+    public User getUSerByUserNameAndPassword(String username, String password) {
+        String query = "select * from users where Username = ? and Password = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return new User(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getDate(5), resultSet.getString(6), resultSet.getString(7), resultSet.getString(8), resultSet.getString(9), resultSet.getBoolean(10), resultSet.getString(11));
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     private boolean login(String username, String password, String query) {
         try {
-            Connection conn = connectDB.connectionDB();
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, username);
             preparedStatement.setString(2, password);
 
