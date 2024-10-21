cdac33dc8fb73512650f1f501b460dd406f125cb
Khuc Nhan <khucvii@gmail.com>
2024-10-21 13:22:56 +0700

    fix something

--- src/main/java/com/example/book_store/Main.java
+++ src/main/java/com/example/book_store/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 860, 480);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
