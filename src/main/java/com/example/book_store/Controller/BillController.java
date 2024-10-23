4f330eabca1563ac87136404a679897597977f23
Khuc Nhan <khucvii@gmail.com>
2024-10-23 08:53:26 +0700

    feature select book in cart and select all book in cart

--- src/main/java/com/example/book_store/Controller/BillController.java
+++ src/main/java/com/example/book_store/Controller/BillController.java
@@ -12,19 +12,4 @@ public class BillController {
     private final ConnectDB connectDB = new ConnectDB();
     private final Connection connection = connectDB.connectionDB();
     private final User currentUser = Authentication.currentUser;
-    public boolean addBill(Date date, double total, int userID) {
-        String query = "insert into Bills (Date, TotalAmount, UserID) values (?, ?, ?)";
-        int row = 0;
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setDate(1, date);
-            preparedStatement.setDouble(2, total);
-            preparedStatement.setInt(3, userID);
-            row = preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return row != 0;
-    }
 }
--- src/main/java/com/example/book_store/Controller/OrderController.java
+++ src/main/java/com/example/book_store/Controller/OrderController.java
@@ -9,21 +9,4 @@ import java.sql.SQLException;
 public class OrderController {
     private final ConnectDB connectDB = new ConnectDB();
     private final Connection connection = connectDB.connectionDB();
-    public boolean addOrder(int billID, int bookID, int amount, double price) {
-        String query = "insert into Orders (BillID, BookID, Amount, Price) select ?, ?, ?, ? from Books where BookID = ?";
-        int row = 0;
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, billID);
-            preparedStatement.setInt(2, bookID);
-            preparedStatement.setInt(3, amount);
-            preparedStatement.setDouble(4, price);
-            preparedStatement.setInt(5, bookID);
-            row = preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return row != 0;
-    }
 }
